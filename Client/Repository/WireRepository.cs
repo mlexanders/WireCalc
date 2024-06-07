38afcba32b417d19985b0e6abd3ddea61703969e
mlexanders <mlexanders@outlook.com>
2024-06-08 00:45:54 +0300

    fix

--- Client/Repository/WireRepository.cs
+++ Client/Repository/WireRepository.cs
@@ -36,7 +36,39 @@ public class WireRepository
             Name = "AC-120/19",
             ActiveResistance = 0.244,
             InductivityResistance = 0.4
-        }
+        },
+        
+        new Wire
+        {
+            Id = 5,
+            Name = "СИП-3х35",
+            ActiveResistance = 0.3, // r
+            InductivityResistance = 0.868   // x
+        },
+        
+        new Wire
+        {
+            Id = 6,
+            Name = "СИП-3х50",
+            ActiveResistance = 0.299, // r
+            InductivityResistance = 0.641   // x
+        },
+        
+        new Wire
+        {
+            Id = 7,
+            Name = "СИП-3х70",
+            ActiveResistance = 0.291, // r
+            InductivityResistance = 0.443   // x
+        },
+
+        new Wire
+        {
+            Id = 8,
+            Name = "СИП-3х95",
+            ActiveResistance = 0.284, // r
+            InductivityResistance = 0.320   // x
+        },
     };
 
     public List<Wire> WiresFromCableLine => new()
